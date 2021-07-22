60636a4590dff743e21a2e0c00c327f32ab0fce4
Alicia <alicia.arenzana@gmail.com>
2021-07-22 13:45:08 +0200

    fixed post and connection with CT

--- api/GeoTrainer/services.py
+++ api/GeoTrainer/services.py
@@ -300,7 +300,6 @@ def min_max_values(image, collection, scale, norm_type='global', geostore=None, 
         
         if norm_type == 'geostore':
             try:
-                #geostore = Skydipper.Geometry(id_hash=geostore_id)
                 features = []
                 for feature in geostore.get('geojson').get('features'):
                     features.append(ee.Feature(feature))
