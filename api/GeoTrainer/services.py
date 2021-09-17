da92a964764073048bde75d74d94e846ff3205cd
Alicia <alicia.arenzana@gmail.com>
2021-09-17 10:29:10 +0200

    update global normalization

--- api/GeoTrainer/services.py
+++ api/GeoTrainer/services.py
@@ -186,8 +186,9 @@ class Preprocessing():
         if 'geostore' in kwargs["sanitized_params"].keys():
             self.geostore_id = str(kwargs['sanitized_params']['geostore'])
         # TODO create geostore from geojson
-        else:
+        elif self.norm_type != 'global':
            try:
+               # we have an issue connecting with geostore
                self.geostore_id = GeostoreService.post(self.geojson)
            except Exception as err:
                return error(status=502, detail=f'{err}')
